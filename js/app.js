50147c3ccc09084e4a881b75c5dee062218ee4c1
Emilse Arias <emilsearias@gmail.com>
2022-06-28 11:26:02 -0300

    agregamos validacion en el select del genero

--- js/app.js
+++ js/app.js
@@ -3,9 +3,10 @@ import {campoRequerido, cantidadCaracteres, validarDNI, validarNumeros} from './
 let nombre = document.getElementById('nombre');
 let edad = document.getElementById('edad');
 let dni = document.getElementById('dni');
+let genero =  document.getElementById('genero')
 
-console.log(nombre)
 nombre.addEventListener('blur',()=>{cantidadCaracteres(2,40,nombre)});
 edad.addEventListener('blur',()=>{validarNumeros(edad)});
 dni.addEventListener('blur',()=>{validarDNI(dni)});
+genero.addEventListener('change', ()=>{campoRequerido(genero)});
 
