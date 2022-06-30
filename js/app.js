ee0e23c4b82d1ba3412c480e3d1dfda6dc975564
Emilse Arias <emilsearias@gmail.com>
2022-06-30 11:51:08 -0300

    agregue detalles para mostrar la generacion y datos de la persona creada

--- js/app.js
+++ js/app.js
@@ -17,8 +17,9 @@ let peso = document.getElementById("peso");
 let altura = document.getElementById("altura");
 let anio = document.getElementById("fechaNacimiento");
 let formulario = document.getElementById("formGeneraciones");
-let alert = document.querySelector('#msjError');
-let btn = document.querySelector('#mostrardatos');
+let alert = document.querySelector("#msjError");
+let btnmostrarDatos = document.querySelector("#mostrarDatos");
+let btnmostrarGeneracion = document.querySelector("#mostrarGeneracion");
 
 nombre.addEventListener("blur", () => {
   cantidadCaracteres(2, 40, nombre);
@@ -37,9 +38,6 @@ altura.addEventListener("blur", () => validarAltura(altura));
 anio.addEventListener("keyup", () => validarAnio(anio));
 anio.addEventListener("blur", () => validarAnio(anio));
 formulario.addEventListener("submit", crearPersona);
-btn.addEventListener("click", ()=>{
-    nuevaPersona.mostrarDatos();
-});
 
 function crearPersona(e) {
   e.preventDefault();
@@ -52,8 +50,8 @@ function crearPersona(e) {
   ) {
     console.log("tengo que crear la persona");
     //hay que ocultar el alert
-    alert.className= 'alert alert-danger mt-3 d-none';
-    let nuevaPersona = new Persona(
+    alert.className = "alert alert-danger mt-3 d-none";
+    const nuevaPersona = new Persona(
       nombre.value,
       edad.value,
       dni.value,
@@ -62,15 +60,42 @@ function crearPersona(e) {
       altura.value,
       anio.value
     );
-    console.log(nuevaPersona);
+    //reseteo los datos del formulario y los detalles
+    resetearDatos();
+    // mostramos opciones para la persona creada
+    let datosExtras = document.querySelector("#datosExtras");
+    datosExtras.className = "container bg-light my-4 rounded-3";
+    // agregar el nombre de la persona en la seccion de detalle
+    datosExtras.children[0].children[0].innerHTML = `<i class="bi bi-person-badge"></i> Persona: ${nuevaPersona.mostrarNombre}`;
+    // opciones para agregar un manejador de eventos en un boton
+    btnmostrarDatos.addEventListener("click", () =>
+      mostrarDatosPersona(nuevaPersona)
+    );
+    btnmostrarGeneracion.addEventListener("click", () => {
+      let panelDatos = document.querySelector("#detalle");
+      panelDatos.innerHTML = nuevaPersona.mostrarGeneracion();
+    });
   } else {
     console.log(
       "mostrar un mensaje de error, para que complete los datos correctamente"
     );
-    alert.innerHTML='Debe completar los datos indicados con *';
-    alert.className= 'alert alert-danger mt-3';
-    setTimeout(()=>{
-        alert.className= 'alert alert-danger mt-3 d-none';
-    },5000)
+    alert.innerHTML = "Debe completar los datos indicados con *";
+    alert.className = "alert alert-danger mt-3";
+    setTimeout(() => {
+      alert.className = "alert alert-danger mt-3 d-none";
+    }, 5000);
   }
+}
+
+function resetearDatos() {
+  formGeneraciones.reset();
+  document.querySelector("#detalle").innerHTML = "";
+  //quitar la clase validate a los input
+  for (let input = 0; input <= 6; input++)
+    formGeneraciones.children[input].children[1].className = "form-control";
+}
+
+function mostrarDatosPersona(persona) {
+  let panelDatos = document.querySelector("#detalle");
+  panelDatos.innerHTML = persona.mostrarDatos();
 }
