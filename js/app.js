513fe35ea20e28b3b73c97c2260ed3c9c445acb0
Emilse Arias <emilsearias@gmail.com>
2022-06-30 11:07:46 -0300

    agregamos todas las validaciones

--- js/app.js
+++ js/app.js
@@ -1,12 +1,31 @@
-import {campoRequerido, cantidadCaracteres, validarDNI, validarNumeros} from './validaciones.js';
+import {campoRequerido, cantidadCaracteres, validarAltura, validarAnio, validarDNI, validarNumeros, validarPeso} from './validaciones.js';
 
 let nombre = document.getElementById('nombre');
 let edad = document.getElementById('edad');
 let dni = document.getElementById('dni');
 let genero =  document.getElementById('genero')
+let peso =  document.getElementById('peso')
+let altura =  document.getElementById('altura')
+let anio =  document.getElementById('fechaNacimiento')
+let formulario =  document.getElementById('formGeneraciones')
 
 nombre.addEventListener('blur',()=>{cantidadCaracteres(2,40,nombre)});
 edad.addEventListener('blur',()=>{validarNumeros(edad)});
 dni.addEventListener('blur',()=>{validarDNI(dni)});
 genero.addEventListener('change', ()=>{campoRequerido(genero)});
+peso.addEventListener('blur', ()=>validarPeso(peso));
+altura.addEventListener('blur', ()=>validarAltura(altura));
+anio.addEventListener('keyup', ()=>validarAnio(anio));
+anio.addEventListener('blur', ()=>validarAnio(anio));
+formulario.addEventListener('submit',crearPersona)
 
+
+function crearPersona(e){
+    e.preventDefault();
+    // console.log(e);
+     if(cantidadCaracteres(2,40,nombre) && validarNumeros(edad) && validarDNI(dni) && validarAnio(anio)){
+        console.log('tengo que crear la persona')
+     }else{
+        console.log('mostrar un mensaje de error, para que complete los datos correctamente')
+     }
+}
--- js/validaciones.js
+++ js/validaciones.js
@@ -11,8 +11,10 @@ export function campoRequerido(input) {
 export function cantidadCaracteres(min, max, input) {
   if (input.value.trim().length >= min && input.value.trim().length <= max) {
     input.className = "form-control is-valid";
+    return true;
   } else {
     input.className += " is-invalid";
+    return false;
   }
 }
 // emilse@gmail.com  xxxxxxxxxxxxxxxxx@xxxxx.xxxx   103, 22, 5
@@ -20,20 +22,55 @@ export function cantidadCaracteres(min, max, input) {
 export function validarNumeros(input) {
   // validar con expresiones regulares
   let patron = /^[0-9]{1,3}$/;
-  console.log(input.value.trim().length)
+  console.log(input.value.trim().length);
   if (patron.test(input.value.trim())) {
     input.className = "form-control is-valid";
+    return true;
   } else {
     input.className += " is-invalid";
+    return false;
   }
 }
 
 // 6 888 777 60 888 777
-export function validarDNI(input){
-    let patron = /^[\d]{1,2}[\d]{3,3}[\d]{3,3}$/;
-    if(patron.test(input.value.trim())){
-        input.className = "form-control is-valid";
-    }else{
-        input.className = "form-control is-invalid";
-    }
+export function validarDNI(input) {
+  let patron = /^[\d]{1,2}[\d]{3,3}[\d]{3,3}$/;
+  if (patron.test(input.value.trim())) {
+    input.className = "form-control is-valid";
+    return true;
+  } else {
+    input.className = "form-control is-invalid";
+    return false;
+  }
+}
+
+export function validarPeso(input) {
+  let patron = /^[\d]{1,3}(\,[\d]{1,2})?$/;
+  if (patron.test(input.value.trim())) {
+    input.className = "form-control is-valid";
+  } else {
+    input.className = "form-control is-invalid";
+  }
+}
+
+export function validarAltura(input) {
+  let patron = /^[1-2]{1}(\,[\d]{1,2})?$/;
+  if (patron.test(input.value.trim())) {
+    input.className = "form-control is-valid";
+  } else {
+    input.className = "form-control is-invalid";
+  }
+}
+
+export function validarAnio(input) {
+  let anioActual = new Date().getFullYear();
+  console.log(anioActual);
+  console.log(input.value);
+  if (input.value >= 1930 && input.value <= anioActual) {
+    input.className = "form-control is-valid";
+    return true;
+  } else {
+    input.className = "form-control is-invalid";
+    return false;
+  }
 }
